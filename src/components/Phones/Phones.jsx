b36d936880191de19a3a5121a226c73ffd499bc6
Rumasultana23 <romaproma23@gmail.com>
2023-09-21 14:01:40 +0600

    added react awsome components

--- src/components/Phones/Phones.jsx
+++ src/components/Phones/Phones.jsx
@@ -6,8 +6,9 @@ import { Audio, Grid } from 'react-loader-spinner'
 
 const Phones = () => {
     const [phones, setPhones] = useState([]);
+    const [loading, setLoading] = useState(true);
 
-    useEffect(() =>{
+    useEffect(() => {
         // fetch('https://openapi.programming-hero.com/api/phones?search=iphone')
         //     .then(res => res.json())
         //     .then(data => setPhones(data.data))
@@ -23,11 +24,33 @@ const Phones = () => {
                     }
                     return obj;
                 })
-                setPhones(phonesWithFakeData)
+                setPhones(phonesWithFakeData);
+                setLoading(false);
             })
     }, [])
     return (
         <div>
+            {loading && <div>
+                <Audio
+                    height="80"
+                    width="80"
+                    radius="9"
+                    color='green'
+                    ariaLabel='three-dots-loading'
+                    wrapperStyle
+                    wrapperClass
+                />
+                <Grid
+                    height="80"
+                    width="80"
+                    color="#4fa94d"
+                    ariaLabel="grid-loading"
+                    radius="12.5"
+                    wrapperStyle={{}}
+                    wrapperClass=""
+                    visible={true}
+                />
+            </div>}
             <h2 className="text-5xl">Phones: {phones.length}</h2>
             <BarChart width={1200} height={400} data={phones}>
                 <Bar dataKey="price" fill="#8884d8" />
